b287f48670af07d9f5278e1555b801335536efd1
PATRICIORICARDI <patricio.ricardi@gmail.com>
2024-11-19 10:10:34 -0600

    Se configura formulario del libro.
     Se configura formulario del libro agregando funciones basicas para pruebas.

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,12 +1,128 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BookService } from '../Services/book.service';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-book-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    RouterModule,
+  ],
   templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.scss'
+  styleUrl: './book-form.component.scss',
 })
 export class BookFormComponent {
+  formBook!: FormGroup;
+  isSaveInProgress: Boolean = false;
+  edit: Boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private bookService: BookService,
+    private activatedRoute: ActivatedRoute,
+    private messageService: MessageService,
+    private router: Router
+  ) {
+    this.formBook = this.fb.group({
+      id: [null],
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      pages: [1, [Validators.required, Validators.min(1)]],
+      price: [0, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id !== 'new') {
+      this.edit = true;
+      this.getBookById(+id!);
+    }
+  }
+
+  getBookById(id: number) {
+    this.bookService.getBookById(id).subscribe({
+      next: (foundBook) => {
+        this.formBook.patchValue(foundBook);
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se encontro el libro',
+        });
+        this.router.navigateByUrl('/');
+      },
+    });
+  }
+
+  saveBook() {
+    if (this.formBook.invalid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'formulario invalido',
+      });
+      return;
+    }
+    this.bookService.saveBook(this.formBook.value).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Guardado',
+          detail: 'Se creo libro correctamente',
+        });
+        this.router.navigateByUrl('/');
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo crear el libro',
+        });
+        this.router.navigateByUrl('/');
+      },
+    });
+  }
+  updateBook() {
+    if (this.formBook.invalid) {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Message Content',
+      });
+    }
+    this.bookService.updateBook(this.formBook.value).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Actualizado',
+          detail: 'Se actualizo el libro correctamente',
+        });
+        this.router.navigateByUrl('/');
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo crear el libro',
+        });
+        this.router.navigateByUrl('/');
+      },
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { Book } from '../Models/book';
+import { BookService } from '../Services/book.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, CardModule],
+  imports: [ButtonModule, CardModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {}
+
+  getAllBooks() {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+    });
+  }
+}
